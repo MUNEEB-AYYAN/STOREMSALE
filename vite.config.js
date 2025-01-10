7c562eda64e63b5c33ab9329e9e5e3bee70e1ac7
MUNEEB-AYYAN <muneebayyan348@gmail.com>
2025-01-11 00:15:17 +0530

    try to be

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@
 
 // // https://vite.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
+  //   plugins: [react()],
+  // })
+  
+  
+  import { defineConfig } from 'vite';
+  import react from '@vitejs/plugin-react';
+  
+  // https://vite.dev/config/   
+  export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
